abcabcf7e94ca4318bad3a0e7c514f805f305616
Haseeb Khan <haseeb_f_khan@outlook.com>
2021-05-09 17:52:45 +0500

    Pages added

--- Router/user.js
+++ Router/user.js
@@ -1,3 +1,33 @@
+// const express = require("express");
+// const router = express.Router();
+
+// router.post("/reaction/angry", (req, res) => {
+//   res.send("Angry reacted");
+// });
+
+// router.post("/reaction/laugh", (req, res) => {
+//   res.send("Laugh reacted");
+// });
+
+// router.post("/reaction/like", (req, res) => {
+//   res.send("Like reacted");
+// });
+
+// // router.get("/getuser", (req, res) => {
+// //   res.status(201).json({
+// //     username: "Haseeb Khan",
+// //   });
+// // });
+// router.get("/:id", (req, res) => {
+//   res.status(201).json({
+//     username: "Haseeb Khan",
+//   });
+// });
+
+// // exporting router so it can be used in api.js
+// module.exports = router;
+// ********************************************************************************************************
+
 const express = require("express");
 const router = express.Router();
 
@@ -18,8 +48,15 @@ router.post("/reaction/like", (req, res) => {
 //     username: "Haseeb Khan",
 //   });
 // });
-router.get("/:id", (req, res) => {
+router.post("/:id", (req, res) => {
+  console.log(req.body);
+  // console.log(req.query);
+  // console.log(req.params.id);
+  console.log(req.params);
+  console.log(req.headers);
+
   res.status(201).json({
+    id: req.params.id,
     username: "Haseeb Khan",
   });
 });
--- api.js
+++ api.js
@@ -1,9 +1,56 @@
+// const express = require("express");
+// const userRouter = require("./Router/user");
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Get method");
+// });
+
+// // using router to complete url of specific path
+// app.use("/user", userRouter);
+
+// // app.post("/", (req, res) => {
+// //   res.send("Post method");
+// // });
+
+// // app.delete("/", (req, res) => {
+// //   res.send("Delete method");
+// // });
+
+// // app.get("/user", (req, res) => {
+// //   //   res.status(204).json({
+// //   //     username: "Haseeb Khan",
+// //   //   });
+// //   res.status(201).json({
+// //     username: "Haseeb Khan",
+// //   });
+// // });
+
+// app.listen(3000, console.log("Server Started!..."));
+// ********************************************************************************************************
+
 const express = require("express");
 const userRouter = require("./Router/user");
 const app = express();
 
+// using express ejs template engine
+app.set("view engine", "ejs");
+
+// express. json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object.
+// This method is called as a middleware in your application using the code: app. use(express. ... urlencoded()
+// is a method inbuilt in express to recognize the incoming Request Object as strings or arrays
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Get method");
+  res.send("/");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact");
 });
 
 // using router to complete url of specific path
